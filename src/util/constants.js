47b8ebaf8ecb41c408c540f5f22b816440d49c1c
baidan <danicabai@163.com>
2019-03-20 20:33:57 +0800

    add files

--- src/util/constants.js
+++
@@ -1,6 +0,0 @@
-const constants = {
-  BROWSER_NAMES: ['Firefox', 'Safari', 'Ubuntu', 'Chrome', 'IE'],
-  ERROR_MSG_BROWSER_NAME: 'Please enter the correct name of browsers'
-}
-
-export default constants
